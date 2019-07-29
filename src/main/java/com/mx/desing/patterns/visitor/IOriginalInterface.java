f2172c3ccef441a30049ab5b1df3b613a1f771db
seeand <kdsfoxy@gmail.com>
2019-07-29 14:41:57 -0600

    Visitor Pattern

---
+++ src/main/java/com/mx/desing/patterns/visitor/IOriginalInterface.java
@@ -0,0 +1,6 @@
+package com.mx.desing.patterns.visitor;
+
+public interface IOriginalInterface {
+
+    void accept(IVisitor visitor);
+}
---
+++ src/main/java/com/mx/desing/patterns/visitor/IVisitor.java
@@ -0,0 +1,6 @@
+package com.mx.desing.patterns.visitor;
+
+public interface IVisitor {
+
+    void visit(MyClass myClassElement);
+}
